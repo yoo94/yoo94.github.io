188779972bddd40ff3752b4593bdc682d3db11ca
yoo94 <ahy0119@naver.com>
2025-06-10 17:42:42 +0900

    css

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,15 @@ document.addEventListener("DOMContentLoaded", () => {
     pageEnterOverlay.className = "page-enter";
     document.body.appendChild(pageEnterOverlay);
 
+    // 페이지 입장 애니메이션 트리거
+    window.addEventListener("load", () => {
+        pageEnterOverlay.style.opacity = "0"; // 밝아지는 애니메이션 트리거
+        setTimeout(() => {
+            pageEnterOverlay.remove(); // 애니메이션 완료 후 제거
+        }, 500); // fadeIn 애니메이션 시간과 동일하게 설정
+    });
+
+    // 페이지 나가기 애니메이션 트리거
     links.forEach(link => {
         link.addEventListener("click", event => {
             event.preventDefault(); // 기본 링크 동작 방지
@@ -16,14 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
             pageExitOverlay.style.opacity = "1"; // 어두워지는 애니메이션 트리거
             setTimeout(() => {
                 window.location.href = href; // 페이지 이동
-            }, 500); // 애니메이션 지속 시간과 동일하게 설정
+            }, 500); // fadeOut 애니메이션 지속 시간과 동일하게 설정
         });
-    });
-
-    window.addEventListener("load", () => {
-        pageEnterOverlay.style.opacity = "0"; // 밝아지는 애니메이션 트리거
-        setTimeout(() => {
-            pageEnterOverlay.remove(); // 애니메이션 완료 후 제거
-        }, 500); // fadeOut 애니메이션 시간과 동일하게 설정
     });
 });
